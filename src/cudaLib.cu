5b994bd3f7339265f3d5e31069017acfece583d1
HoSun Choi <choi1012@purdue.edu>
2025-01-26 22:42:39 -0500

    added GPU SAXPY code

--- src/cudaLib.cu
+++ src/cudaLib.cu
@@ -1,5 +1,6 @@
 
 #include "cudaLib.cuh"
+#include "cpuLib.h"
 
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort)
 {
@@ -13,6 +14,8 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort)
 __global__ 
 void saxpy_gpu (float* x, float* y, float scale, int size) {
 	//	Insert GPU SAXPY kernel code here
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	if (i < size) y[i] = scale * x[i] + y[i];
 }
 
 int runGpuSaxpy(int vectorSize) {
@@ -20,8 +23,65 @@ int runGpuSaxpy(int vectorSize) {
 	std::cout << "Hello GPU Saxpy!\n";
 
 	//	Insert code here
-	std::cout << "Lazy, you are!\n";
-	std::cout << "Write code, you must\n";
+	int vectorBytes = vectorSize * sizeof(float);
+	float *a, *b, *c;
+	float *a_d, *b_d, *c_d;
+
+	// Initialize vectors in CPU
+	a = (float*)malloc(vectorBytes);
+	b = (float*)malloc(vectorBytes);
+	c = (float*)malloc(vectorBytes);
+	if (a == NULL || b == NULL || c == NULL) {
+		printf("Unable to malloc memory ... Exiting!");
+		return -1;
+	}
+	vectorInit(a, vectorSize);
+	vectorInit(b, vectorSize);
+	std::memcpy(c, b, vectorSize * sizeof(float));
+	float scale = 2.0f;
+
+	// Transfer from host to device
+	cudaMalloc((void**) &a_d, vectorBytes);
+	cudaMalloc((void**) &b_d, vectorBytes);
+	cudaMalloc((void**) &c_d, vectorBytes);
+	cudaMemcpy(a_d, a, vectorBytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(b_d, b, vectorBytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(c_d, c, vectorBytes, cudaMemcpyHostToDevice);
+
+	#ifndef DEBUG_PRINT_DISABLE 
+		printf("\n Adding vectors : \n");
+		printf(" scale = %f\n", scale);
+		printf(" a = { ");
+		for (int i = 0; i < 5; ++i) {
+			printf("%3.4f, ", a[i]);
+		}
+		printf(" ... }\n");
+		printf(" b = { ");
+		for (int i = 0; i < 5; ++i) {
+			printf("%3.4f, ", b[i]);
+		}
+		printf(" ... }\n");
+	#endif
+
+	// Kernel code
+	saxpy_gpu<<<ceil(vectorSize/256.0), 256>>>(a_d, c_d, scale, vectorSize);
+
+	// Transfer from device to host
+	cudaMemcpy(c, c_d, vectorBytes, cudaMemcpyDeviceToHost);
+	// Free device memory
+	cudaFree(a_d); cudaFree(b_d); cudaFree(c_d);
+
+	#ifndef DEBUG_PRINT_DISABLE 
+		printf(" c = { ");
+		for (int i = 0; i < 5; ++i) {
+			printf("%3.4f, ", c[i]);
+		}
+		printf(" ... }\n");
+	#endif
+
+	int errorCount = verifyVector(a, b, c, scale, vectorSize);
+	std::cout << "Found " << errorCount << " / " << vectorSize << " errors \n";
+
 
 	return 0;
 }
